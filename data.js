2a2548d24c0e05794866a11373e1f647f4d574e4
Ram <ramakantsharma272154@gmail.com>
2026-03-11 07:18:20 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,22 @@
+export const cards = [
+{
+title:"Healthy Apple",
+type:"good",
+image:"https://picsum.photos/300"
+},
+{
+title:"Burger",
+type:"junk",
+image:"https://picsum.photos/301"
+},
+{
+title:"Salad",
+type:"good",
+image:"https://picsum.photos/302"
+},
+{
+title:"Pizza",
+type:"junk",
+image:"https://picsum.photos/303"
+}
+]
